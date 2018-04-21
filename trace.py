8a6097e919edb9cfa64ea64180acde3c2580555c
Luke Hewitt <insperatum@gmail.com>
2018-04-21 17:46:42 -0400

    Experiment 1524347202010

--- trace.py
+++ trace.py
@@ -565,7 +565,7 @@ class Trace:
 		if conceptClass is PYConcept: conceptTypePrior = math.log(0.5)
 
 		self.score += math.log(self.model.geom_p) + conceptTypePrior + prior
-		print(self.model.geom_p, conceptTypePrior, prior)
+		print(self.model.geom_p, conceptTypePrior, prior, flush=True)
 		for c in conceptsReferenced:
 			if c in self.baseConcepts:
 				self.score += self.logpConcept(c)
