3c5452594385e2400ac615e4d2407a6f5dae818e
Luke Hewitt <insperatum@gmail.com>
2018-05-09 15:22:03 -0400

    Script to render all iterations at once

---
+++ render_all.py
@@ -0,0 +1,9 @@
+import loader
+import os
+
+for model in ('results/%s'%x for x in os.listdir('results') if x[-3:]==".pt"):
+	print("Loading", model)
+	M = loader.load(model)
+	loader.saveRender(M)
+
+print("finished.")
