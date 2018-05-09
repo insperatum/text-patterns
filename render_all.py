3cc6eb27dcf416a91093dad83c5783e594050f4a
Luke Hewitt <insperatum@gmail.com>
2018-05-08 23:05:27 -0400

    work in progress

--- render_all.py
+++ render_all.py
@@ -1,16 +1,7 @@
 import loader
-from propose import Proposal, evalProposal, getProposals, networkCache, getNetworkRegexes
-import util
-
-import torch
-
 import os
-import math
-import argparse
-import pregex as pre
-from trace import RegexWrapper
 
 for model in ('results/%s'%x for x in os.listdir('results') if x[-3:]==".pt"):
-	print("Loading", args.model)
-	M = loader.load(args.model)
+	print("Loading", model)
+	M = loader.load(model)
 	loader.saveRender(M)
