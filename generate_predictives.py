9003e2009b17e644a044c557df80a6affba73afd
Luke Hewitt <insperatum@gmail.com>
2018-05-13 13:02:18 -0400

    work in progress

--- generate_predictives.py
+++ generate_predictives.py
@@ -7,17 +7,10 @@ import torch
 
 import os
 import math
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default=max(('results/%s'%x for x in os.listdir('results') if x[-3:]==".pt"), key=os.path.getmtime)) #Most recent model
-parser.add_argument('--slow', dest='slow', action='store_const', const=True)
-parser.set_defaults(slow=False)
-args = parser.parse_args()
 
 for model in ('results/%s'%x for x in os.listdir('results') if x[-3:]==".pt" and 'no_net' not in x):
 	print("\nModel:", model)
-	M = loader.load(args.model)
+	M = loader.load(model)
 	if torch.cuda.is_available(): M['net'].cuda()
 
 	defaultExamples = [
@@ -30,11 +23,7 @@ for model in ('results/%s'%x for x in os.listdir('results') if x[-3:]==".pt" and
 		["Thur at 14:00"]
 	]
 	for examples in defaultExamples:
-		if args.slow:
-			proposals = list(getProposals(M['net'], M['trace'], examples, nProposals=50, maxNetworkEvals=100, doPrint=False))
-		else:
-			proposals = list(getProposals(M['net'], M['trace'], examples, doPrint=False))
-		j=0
+		proposals = list(getProposals(M['net'], M['trace'], examples, nProposals=50, maxNetworkEvals=100, doPrint=False))
 
 		totalJoint = util.logsumexp([x.final_trace.score for x in proposals])
 		probs = [math.exp(x.final_trace.score - totalJoint) for x in proposals]
