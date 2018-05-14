744356d88e651af8ab56794136a03da4b62a2a9e
Luke Hewitt <insperatum@gmail.com>
2018-05-14 16:18:32 -0400

    work in progress

--- train.py
+++ train.py
@@ -351,6 +351,9 @@ def addTask(task_idx):
 	else:
 		accepted = max(solutions, key=lambda evaluatedProposal: evaluatedProposal.final_trace.score)
 		print("Accepted proposal: " + accepted.concept.str(accepted.trace) + "\nScore:" + str(accepted.final_trace.score - M['trace'].score) + "\n")
+		print("\nBest candidates were:")
+		for p in sorted(solutions, key=lambda evaluatedProposal: evaluatedProposal.final_trace.score, reverse=True)[:10]:
+			print(p.concept.str(accepted.trace, depth=3) + "\nScore:" + str(p.final_trace.score - M['trace'].score) + "\n")
 		M['trace'] = accepted.final_trace
 		M['task_observations'][task_idx] = accepted.observations
 		M['task_concepts'][task_idx] = accepted.concept
