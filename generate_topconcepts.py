db4d26dd9ea33651bff4dbab7beff7f1d8e02a6c
Luke Hewitt <insperatum@gmail.com>
2018-05-16 06:47:38 -0400

    work in progress

--- generate_topconcepts.py
+++ generate_topconcepts.py
@@ -15,7 +15,7 @@ def latexify(s):
 print("\\begin{tabular}{" + " ".split("l"*nSamples) + "}")
 for c in sorted(trace.baseConcepts, key=lambda c: trace.baseConcept_nReferences.get(c,0), reverse=True)[:10]:
 	samples = [c.sample(trace) for _ in range(nSamples)]
-	print(latexify(", ".join(samples)) + "\\")
+	print(latexify(", ".join(samples)) + "\\\\")
 print("\\end{tabular}")
 #print()
 #for model in models:
