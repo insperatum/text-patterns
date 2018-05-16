362ba4845cdb8f50bdfc8f5495f059c16f6d79ff
Luke Hewitt <insperatum@gmail.com>
2018-05-16 00:49:58 -0400

    work in progress

--- train.py
+++ train.py
@@ -362,14 +362,17 @@ def addTask(task_idx):
 
 		try:
 			pq_main.put(q_main.get(timeout=0.1))
+			print(">Got from q_main")
 			continue
 		except queue.Empty:
+			print(">Nothing in p_main. Checking pq_main?")
 			try:
 				queue_item = pq_main.get_nowait()
+				print(">got from pq_main")
 			except queue.Empty:
+				print(">nothing in pq_main")
 				continue
 	
-		print(">Got queue item:", queue_item)
 
 		assert(queue_item.type in ["solution", "partialSolution", "counterexamples"])
 		#Solutions
