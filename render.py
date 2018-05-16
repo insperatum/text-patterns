c49ba7c808fb95f31d5be149909b81d1e153504c
Luke Hewitt <insperatum@gmail.com>
2018-05-16 08:29:09 -0400

    work in progress

--- render.py
+++ render.py
@@ -110,7 +110,7 @@ def saveConcepts(M, filename, onlyIdxs=None, mode="samples"):
 		if isRegex and concept.id != 0:
 			content_prefix = "<font point-size='%d'>: "%(int(size*1.5)) + html_escape(concept.str(trace, depth=1, include_self=False)) + "</font>"
 		else:
-			content_prefix = "<font point-size='%'>: "%(int(size*1.5)) + "(" + concept.str(trace, depth=1, include_self=False)) + ")</font>"
+			content_prefix = "<font point-size='%'>: "%(int(size*1.5)) + "(" + concept.str(trace, depth=1, include_self=False) + ")</font>"
 
 		nTaskReferences = trace.baseConcept_nTaskReferences.get(concept, 0)
 		nConceptReferences = trace.baseConcept_nReferences.get(concept, 0)
