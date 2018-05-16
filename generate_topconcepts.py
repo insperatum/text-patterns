0443cd5833f74034fbdaf1d99368e7ab5928f38e
Luke Hewitt <insperatum@gmail.com>
2018-05-16 07:12:18 -0400

    work in progress

--- generate_topconcepts.py
+++ generate_topconcepts.py
@@ -1,5 +1,6 @@
 import loader
 
+import render
 
 nSamples=6
 
@@ -12,15 +13,18 @@ trace = M['trace']
 def latexify(s):
 	return "\\verb|" + s + "|"
 
+topConcepts = sorted(trace.baseConcepts, key=lambda c: trace.baseConcept_nReferences.get(c,0), reverse=True)[:10]
 print("\\begin{tabular}{l|" + " ".join("l"*nSamples) + "}")
 #print("&\\textbf{Reuses} & \\textbf{Samples}\\\\")
 print(" & Samples\\\\")
 print("\\hline")
-for c in sorted(trace.baseConcepts, key=lambda c: trace.baseConcept_nReferences.get(c,0), reverse=True)[:10]:
+for c in topConcepts: 
 	samples = [c.sample(trace) for _ in range(nSamples)]
 	#print(trace.baseConcept_nReferences.get(c,0), "&", latexify(", ".join(samples)) + "\\\\")
-	print("\\textbf{", c, "}", "&", latexify(", ".join(samples)) + "\\\\")
+	print("\\textbf{\\#" + str(c.id) + "}", "&", latexify(", ".join(samples)) + "\\\\")
 print("\\end{tabular}")
+
+render.saveConcepts(M, M['save_to']+"render_topconcepts.gv", onlyIdxs=[x.id for x in topConcepts])
 #print()
 #for model in models:
 #	print(results[model])
