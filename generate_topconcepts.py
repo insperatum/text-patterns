5aa7e7863d4b56b1820e332ef2a06ac54d51d35d
Luke Hewitt <insperatum@gmail.com>
2018-05-16 06:53:27 -0400

    work in progress

--- generate_topconcepts.py
+++ generate_topconcepts.py
@@ -12,10 +12,11 @@ trace = M['trace']
 def latexify(s):
 	return "\\verb|" + s + "|"
 
-print("\\begin{tabular}{" + " ".join("l"*nSamples) + "}")
+print("\\begin{tabular}{r|" + " ".join("l"*nSamples) + "}")
+print("\\textbf{# times referenced}&\\\\")
 for c in sorted(trace.baseConcepts, key=lambda c: trace.baseConcept_nReferences.get(c,0), reverse=True)[:10]:
 	samples = [c.sample(trace) for _ in range(nSamples)]
-	print(latexify(", ".join(samples)) + "\\\\")
+	print(trace.baseConcept_nReferences.get(c,0), "&", latexify(", ".join(samples)) + "\\\\")
 print("\\end{tabular}")
 #print()
 #for model in models:
