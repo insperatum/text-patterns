5054a3922d8fef4553545f28ec77be15f3409d8f
Luke Hewitt <insperatum@gmail.com>
2018-05-16 06:45:43 -0400

    work in progress

--- generate_topconcepts.py
+++ generate_topconcepts.py
@@ -12,9 +12,9 @@ trace = M['trace']
 def latexify(s):
 	return "\\verb|" + s + "|"
 
-for c in sorted(trace.baseConcepts, key=lambda c: trace.baseConcept_nReferences[c], reverse=True):
-	samples = [c.sample(trace) for _ in range(5)]
-	print(c + "& " +  latexify(", ".join(samples)) + "\\")
+for c in sorted(trace.baseConcepts, key=lambda c: trace.baseConcept_nReferences.get(c,0), reverse=True):
+	samples = [c.sample(trace) for _ in range(nSamples)]
+	print(latexify(", ".join(samples)) + "\\")
 
 #print()
 #for model in models:
