32bdb550ac18601f4476a7caf249bdef5ce6364f
Luke Hewitt <insperatum@gmail.com>
2018-05-16 06:42:34 -0400

    work in progress

---
+++ generate_topconcepts.py
@@ -0,0 +1,22 @@
+import loader
+
+
+nSamples=3
+
+model = "results/model.pt"
+
+print("\nModel:", model)
+M = loader.load(model)
+trace = M['trace']
+
+def latexify(s):
+	return "\\verb|" + s + "|"
+
+for c in sorted(trace.baseConcepts, keys=lambda c: trace.baseConcept_nReferences[c], reverse=True):
+	samples = [c.sample(trace) for _ in range(5)]
+	print(c + "& " +  latexify(", ".join(samples)) + "\\")
+
+#print()
+#for model in models:
+#	print(results[model])
+	
